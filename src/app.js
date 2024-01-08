f8e880e63465668dab03c6fa5195ca9cd9b1ea6b
Yashpal Chouhan <yashstar0009@gmail.com>
2024-01-08 22:06:28 +0530

    added;

--- src/app.js
+++ src/app.js
@@ -11,6 +11,9 @@ app.use(express.json({limit : '16kb'}));
 app.use(express.urlencoded({extended: true, limit :'16kb'}));
 app.use("/screen-share",screenShare);
 
+app.get("/",(req,res)=>{
+    res.send("working");
+})
 
 
 export {
