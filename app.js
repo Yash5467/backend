459d1f2d9d687459e2fed9dda6fdd51e9f48eb75
Yashpal Chouhan <yashstar0009@gmail.com>
2024-01-08 23:20:28 +0530

    updated env;

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ import {screenShare} from './routes/index.js'
 const app=express();
 
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_SIDE_URL
+}));
 app.use(express.json({limit : '16kb'}));
 
 app.use(express.urlencoded({extended: true, limit :'16kb'}));
